f90145b85858f560997f3d61f3ae47f9b7f29616
papaamadousarr <papaamadou53@gmail.com>
2024-08-28 19:38:34 +0000

    graphql

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
         })
         .then(data => {
             console.log('Authentication Response:', data); // Pour vérifier la structure de la réponse
-            if (data.token) {
-                localStorage.setItem('jwt', data.token);
+            if (data) {
+                localStorage.setItem('jwt', data);
                 showProfile();
             } else {
                 throw new Error('No token received');
